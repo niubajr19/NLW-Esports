079a7dfe4aa692b1b91ae8f444d803b2d303e3d4
Niubajr19 <lucas.antonio199@outlook.com>
2022-09-13 19:19:41 -0300

    Home - Finalizada listagem dos games

---
+++ mobile/src/components/GameCard/index.tsx
@@ -0,0 +1,35 @@
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { TouchableOpacityProps, Text } from "react-native";
+import {
+  TouchableOpacity,
+  ImageBackground,
+  ImageSourcePropType,
+} from "react-native";
+import { THEME } from "../../theme";
+
+import { styles } from "./styles";
+
+interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({ data }: Props) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <ImageBackground style={styles.cover} source={data.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
